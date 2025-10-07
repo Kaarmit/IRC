0429aea9139bdd2a02c4d373cd99305109784951
Daryl Avril <daavril@f1r11s16.clusters.42paris.fr>
2025-10-07 15:52:54 +0200

    test inutile

--- channel/chanell.hpp
+++ channel/chanell.hpp
@@ -3,14 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   chanell.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aarmitan <aarmitan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: daavril <daavril@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 14:47:06 by aarmitan          #+#    #+#             */
-/*   Updated: 2025/10/07 14:47:22 by aarmitan         ###   ########.fr       */
+/*   Updated: 2025/10/07 15:52:23 by daavril          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CHANNEL_HPP
 #define CHANNEL_HPP
+
+#include <iostream>
+
+typedef struct chanel
+{
+	std::string	fd;
+};
+
 
 #endif
