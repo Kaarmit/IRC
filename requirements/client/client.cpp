6565e4659f22af85692a1a5033a181b3854fb68e
Aicha Stierlen <aistierl@f2r5s6.clusters.42paris.fr>
2025-10-13 17:16:15 +0200

    added getter and setter to client class

--- requirements/client/client.cpp
+++ requirements/client/client.cpp
@@ -6,12 +6,25 @@ client::client()
 }
 
 client::client(int clientFd): _fd(clientFd) {
+	this->_registered = false;
 }
 
 client::~client()
 {
 }
 
+int				client::getFd(void) const {
+	return (this->_fd);
+}
+
 message*		client::getMessage(void) const {
 	return (this->_msg);
+}
+
+bool			client::getRegistered(void) const {
+	return (this->_registered);
+}
+
+void			client::setRegistered(bool status) {
+	this->_registered = status;
 }
