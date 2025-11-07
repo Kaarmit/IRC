091a49659c4c4bdbb5af78b941da3296f824423f
Kaarmit <165493847+Kaarmit@users.noreply.github.com>
2025-11-07 10:03:20 +0100

    Refactor handleUser for better parameter validation
    Refactor handleUser function to improve parameter handling and validation.

--- requirements/server/commands/auth/user.cpp
+++ requirements/server/commands/auth/user.cpp
@@ -12,45 +12,65 @@
 
 #include "../../server.hpp"
 
-bool	server::handleUser(client* cli, message& msg)
+bool server::handleUser(client* cli, message& msg)
 {
-	const std::string target = cli->getNick().empty() ? "*" : cli->getNick();
+    const std::string target = cli->getNick().empty() ? "*" : cli->getNick();
 
-	if (!basicChecks(cli, msg))
-		return false;
-
-
-	const std::string username = msg.getParams()[0];
-	// std::cout << "LOG: username: " << username << std::endl;
-	// std::cout << "LOG: hostname: " << msg.getParams()[1] << std::endl;
-	// std::cout << "LOG: servername: " << msg.getParams()[2] << std::endl;
-	// std::cout << "LOG: realname: " << msg.getParams()[3] << std::endl;
-
-    if (!isValidUsername(username))
-    {
-        std::string line = ":" + _serverName + " 468 " + target +  " :Erroneous username\r\n";
+    if (cli->getRegistered())
+	{
+		std::string line = ":" + _serverName + " 462 " + target + " :You may not reregister\r\n";
         cli->enqueueLine(line);
         polloutActivate(cli);
-		// std::cout << "LOG: return false dans user() a la verif 3" << std::endl;
+		// std::cout << "LOG: return false dans user() a la verif 1" << std::endl;
+        return false;
+	}
+    if (msg.getParams().empty())
+    {
+        std::string line = ":" + _serverName + " 431 " + target + " :No username given\r\n";
+        cli->enqueueLine(line);
+        polloutActivate(cli);
+		// std::cout << "LOG: return false dans nick() a la verif 1" << std::endl;
         return false;
     }
 
-	//const std::string hostname = cli->getHost();
-	//const std::string servername = this->_serverName;
-	std::string realname = msg.getParams()[3];
+    const std::vector<std::string>& p = msg.getParams();
 
-	for (size_t i = 4; i < msg.getParams().size(); i++)
-	{
-		realname = realname + " " + msg.getParams()[i];
-	}
-	if (!realname.empty() && realname[0] == ':')
-		realname.erase(0,1);
-	cli->setUser(username);
-	cli->setReal(realname);
+    // USER <username> <mode> <unused> <realname...>
+    if (p.size() < 4) {
+        std::string line = ":" + _serverName + " 461 " + target + " USER :Not enough parameters\r\n";
+        cli->enqueueLine(line);
+        polloutActivate(cli);
+        return false;
+    }
 
-	if (!cli->getRegistered() && !cli->getNick().empty() && !cli->getUser().empty() && !cli->getPass().empty())
-		sendWelcomeIfRegistrationComplete(cli);
-	// std::cout << "LOG: TA GRAND MERE PASSe DANS USER" << std::endl;
+    const std::string& username = p[0];
+
+    if (!isValidUsername(username)) {
+        std::string line = ":" + _serverName + " 468 " + target + " :Erroneous username\r\n";
+        cli->enqueueLine(line);
+        polloutActivate(cli);
+        return false;
+    }
+
+    // Concaténer realname à partir de p[3], gérer ':' optionnel, trim espaces/CRLF
+    std::string realname = p[3];
+    for (size_t i = 4; i < p.size(); ++i) {
+        realname += " ";
+        realname += p[i];
+    }
+    if (!realname.empty() && realname[0] == ':')
+        realname.erase(0, 1);
+    while (!realname.empty() && (realname[0] == ' ' || realname[0] == '\t'))
+        realname.erase(0, 1);
+    for (std::string::size_type i = 0; i < realname.size(); )
+        if (realname[i] == '\r' || realname[i] == '\n') realname.erase(i, 1); else ++i;
+    if (realname.empty()) realname = "unknown";
+
+    cli->setUser(username);
+    cli->setReal(realname);
+
+    if (!cli->getRegistered() && !cli->getNick().empty() && !cli->getUser().empty() && !cli->getPass().empty())
+        sendWelcomeIfRegistrationComplete(cli);
+
     return true;
-
 }
